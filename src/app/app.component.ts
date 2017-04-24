61c6ac18d8fc369001ee479127f0a8c1a20c7a89
camilolozano <kamilo92@live.com>
2017-04-23 21:36:51 -0500

    initial commit heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../../node_modules/leaflet/dist/leaflet.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  ngOnInit() {
+    let map = L.map('map').setView([51.505, -0.09], 13);
+    const URL_MAP = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2FtaWxvbG96YW5vIiwiYSI6IndjZ2VPS2cifQ.tphor4E05iEg3b19nfN3Dg';
+    L.tileLayer(URL_MAP, {
+      maxZoom: 10,
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      id: 'mapbox.streets'
+    }).addTo(map);
+
+  }
 }
